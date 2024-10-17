820e46a8f0c5479139b55801be93713fe42db858
saju03 <sajusuresh0752@gmail.com>
2024-10-18 02:38:23 +0530

    generative ai added git file issue fix

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -103,9 +103,9 @@ const Table  = () => {
                     <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       Amount
                     </th>
-                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    {/* <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                       Action
-                    </th>
+                    </th> */}
                   </tr>
                 </thead>
 
